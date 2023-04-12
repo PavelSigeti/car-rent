a28f9ae356d72097995f106d5675a8fb722ffeba
PavelSigeti <sigeti352@gmail.com>
2023-04-12 21:30:56 +0300

    Docker upd

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM php:8.1.9-fpm
 
+COPY composer.lock composer.json
 # WORKDIR /var/www
 
 RUN apt-get update && apt-get install -y \
@@ -17,17 +18,22 @@ RUN apt-get update && apt-get install -y \
     mc \
     unzip \
     git \
-    curl
+    curl \
+    libmagickwand-dev --no-install-recommends && rm -rf /var/lib/apt/lists/*
 
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
 
 RUN docker-php-ext-install pdo_mysql mbstring zip exif pcntl
 RUN docker-php-ext-configure gd --with-freetype --with-jpeg
 RUN docker-php-ext-install gd
+RUN printf "\n" | pecl install imagick
+RUN docker-php-ext-enable imagick
 
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 RUN chown -R www-data:www-data /var/www
 
+
 EXPOSE 9000
-CMD ["php-fpm"]
+
+CMD ["sh", "-c", "composer install && php artisan migrate && php artisan storage:link && php-fpm"]
--- app/Http/Middleware/CollectData.php
+++ app/Http/Middleware/CollectData.php
@@ -19,8 +19,7 @@ class CollectData
     public function handle(Request $request, Closure $next)
     {
         $data = $request->server();
-
-        $bot = Bot::query()->where('ip', $data['HTTP_X_REAL_IP'])->exists();
+        $bot = Bot::query()->where('ip', $_SERVER['REMOTE_ADDR'])->exists();
 
         if($bot) {
             return redirect()->route('verify');
@@ -30,7 +29,7 @@ class CollectData
 
         UserData::query()->create(
             [
-                'HTTP_X_REAL_IP' => isset($data['HTTP_X_REAL_IP']) ? $data['HTTP_X_REAL_IP'] : null,
+                'HTTP_X_REAL_IP' => isset($data['REMOTE_ADDR']) ? $data['REMOTE_ADDR'] : null,
                 'HTTP_USER_AGENT' => isset($data['HTTP_USER_AGENT']) ? $data['HTTP_USER_AGENT'] : null,
                 'HTTP_ACCEPT_LANGUAGE' => isset($data['HTTP_ACCEPT_LANGUAGE']) ? $data['HTTP_ACCEPT_LANGUAGE'] : null,
                 'HTTP_COOKIE' => $cookie,
